0a43eaee864d4e0d011ac0e3096854aa4e51c793
Joschka Birk <joschka.birk@gmail.com>
2025-07-24 15:46:29 +0200

    Update job scripts

--- examples/job_scripts/example_job_train_classification.sh
+++ examples/job_scripts/example_job_train_classification.sh
@@ -9,7 +9,7 @@
 #SBATCH --constraint=GPU
 #SBATCH --time=48:00:00
 #SBATCH --job-name TrainClass
-#SBATCH --output /beegfs/desy/user/birkjosc/gabbro_output/logs/slurm_logs/%x_%j.log      # ADJUST THIS to your log path
+#SBATCH --output /data/dust/user/birkjosc/beegfs/gabbro_output//logs/slurm_logs/%x_%j.log      # ADJUST THIS to your log path
 #SBATCH --mail-user <your-email>
 
 echo "Starting job $SLURM_JOB_ID with the following script:"
@@ -22,14 +22,14 @@ export PYTHONPATH="${REPO_DIR}:${PYTHONPATH}"
 
 cd $REPO_DIR
 
-LOGFILE="/beegfs/desy/user/birkjosc/gabbro_output/logs/slurm_logs/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.log"  # ADJUST THIS to your log path
+LOGFILE="/data/dust/user/birkjosc/beegfs/gabbro_output//logs/slurm_logs/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.log"  # ADJUST THIS to your log path
 
 PYTHON_COMMAND="python gabbro/train.py experiment=example_experiment_classification"
 
 # run the python command in the singularity container
 # ADJUST THIS to your singularity image path, or replace with docker://jobirk/omnijet:latest
-singularity exec --nv --bind /beegfs:/beegfs \
-    --env SLURM_JOB_ID="$SLURM_JOB_ID" --env SLURM_LOGFILE="$LOGFILE" \
+singularity exec --nv -B /data  \
+    --env JOB_ID="$SLURM_JOB_ID" --env SLURM_LOGFILE="$LOGFILE" \
     /beegfs/desy/user/birkjosc/singularity_images/omnijet-latest.sif \
     bash -c "source /opt/conda/bin/activate && $PYTHON_COMMAND"
 
--- examples/job_scripts/example_job_train_generative.sh
+++ examples/job_scripts/example_job_train_generative.sh
@@ -6,11 +6,11 @@
 ## SLURM JOB COMMANDS ###
 #########################
 #SBATCH --partition=maxgpu
-#SBATCH --constraint=GPU
-#SBATCH --constraint="A100|V100"
-#SBATCH --time=48:00:00
+#SBATCH --constraint="GPU"
+#SBATCH --constraint="V100&GPUx4"
+#SBATCH --time=100:00:00
 #SBATCH --job-name GenTrain
-#SBATCH --output /beegfs/desy/user/birkjosc/gabbro_output/logs/slurm_logs/%x_%j.log      # ADJUST THIS to your log path
+#SBATCH --output /data/dust/user/birkjosc/beegfs/gabbro_output//logs/slurm_logs/%x_%j.log      # ADJUST THIS to your log path
 #SBATCH --mail-user <your-email-address>  # ADJUST THIS to your email address
 
 echo "Starting job $SLURM_JOB_ID with the following script:"
@@ -22,17 +22,21 @@ export REPO_DIR="/home/birkjosc/repositories/omnijet_alpha/"  # ADJUST THIS to y
 export PYTHONPATH="${REPO_DIR}:${PYTHONPATH}"
 
 cd $REPO_DIR
-
-LOGFILE="/beegfs/desy/user/birkjosc/gabbro_output/logs/slurm_logs/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.log"  # ADJUST THIS to your log path
+LOGFILE="/data/dust/user/birkjosc/beegfs/gabbro_output//logs/slurm_logs/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.log"  # ADJUST THIS to your log path
 
 PYTHON_COMMAND="python gabbro/train.py experiment=example_experiment_generative"
 
 # run the python command in the singularity container
 # ADJUST THIS to your singularity image path, or replace with docker://jobirk/omnijet:latest
-singularity exec --nv --bind /beegfs:/beegfs \
-    --env SLURM_JOB_ID="$SLURM_JOB_ID" --env SLURM_LOGFILE="$LOGFILE" \
-    /beegfs/desy/user/birkjosc/singularity_images/omnijet-latest.sif \
-    bash -c "source /opt/conda/bin/activate && $PYTHON_COMMAND"
+singularity exec --nv \
+    --containall \
+    --bind $REPO_DIR:$REPO_DIR \
+    --bind /data:/data \
+    --bind /home/birkjosc/.netrc:/home/birkjosc/.netrc \
+    --env JOB_ID="$SLURM_JOB_ID" --env SLURM_LOGFILE="$LOGFILE" \
+    --env HOSTNAME="$HOSTNAME" \
+    /data/dust/user/birkjosc/singularity_images/gabbro-v0.2.5.sif \
+    bash -c "source /opt/conda/bin/activate && export PYTHONPATH=${REPO_DIR}:${PYTHONPATH} && cd $REPO_DIR && $PYTHON_COMMAND"
 
 ## ---------------------- End of job script -----------------------------------
 ################################################################################
