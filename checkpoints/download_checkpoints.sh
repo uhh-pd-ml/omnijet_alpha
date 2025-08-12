72111b51c20050b30461ad134421cba44f12c541
Joschka Birk <joschka.birk@gmail.com>
2025-08-12 18:10:50 +0200

    Update link for sync&share, which was broken after a nextcloud update

--- checkpoints/download_checkpoints.sh
+++ checkpoints/download_checkpoints.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl --output checkpoints.tar https://syncandshare.desy.de/index.php/s/JYiSxBTo6FQGXqk/download\?path\=\&files\=checkpoints.tar
+curl --output checkpoints.tar https://syncandshare.desy.de/public.php/dav/files/HX388YZbjSGPNcC/?accept=zip
 tar -xvf checkpoints.tar
 rm -rf checkpoints.tar
